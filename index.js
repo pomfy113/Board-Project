5c29f6eed2b978d3ae7750b0a3116016c86927be
pomfy113 <pomfy113@gmail.com>
2018-01-19 11:52:45 -0800

    Basic set-up

---
+++ index.js
@@ -0,0 +1,21 @@
+// Set-up
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+// View Engine - Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Porting
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
